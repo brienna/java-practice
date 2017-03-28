4a4f5179dd640508dd924ef326e35289c1eb9967
Brienna Herold <brienna.herold@gmail.com>
2017-03-28 19:38:07 -0400

    Add Exercise 17-1 source code

---
+++ murach_eclipse/src/chapter_17/ch17_ex1_CustomerManager/ui/StringUtils.java
@@ -0,0 +1,16 @@
+package chapter_17.ch17_ex1_CustomerManager.ui;
+
+public class StringUtils {
+
+    public static String padWithSpaces(String s, int length) {
+        if (s.length() < length) {
+            StringBuilder sb = new StringBuilder(s);
+            while (sb.length() < length) {
+                sb.append(" ");
+            }
+            return sb.toString();
+        } else {
+            return s.substring(0, length);
+        }
+    }
+}
