c01c49cf8c95c63e5265a2d61914600597cdfb64
Brienna Herold <brienna.herold@gmail.com>
2017-04-03 19:20:02 -0400

    Add I/O code to Exercise 17-1

--- murach_eclipse/src/chapter_17/ch17_ex1_CustomerManager/io/CustomerTextFile.java
+++ murach_eclipse/src/chapter_17/ch17_ex1_CustomerManager/io/CustomerTextFile.java
@@ -7,9 +7,9 @@ import chapter_17.ch17_ex1_CustomerManager.business.Customer;
 
 public class CustomerTextFile {
     private static final String FIELD_SEP = "\t";
-    private static final Path customersPath = null;      // add code here
-    private static final File customersFile = null;      // add code here
-    private static ArrayList<Customer> customers = null; // add code here
+    private static final Path customersPath = Paths.get("customers.txt");      
+    private static final File customersFile = customersPath.toFile();      
+    private static List<Customer> customers = getCustomers(); 
     
     // prevent instantiation of the class
     private CustomerTextFile() {}
@@ -17,15 +17,33 @@ public class CustomerTextFile {
     public static List<Customer> getCustomers() {
         // if the customers file has already been read, don't read it again
         if (customers != null)
-            return customers;        
+            return customers;
 
-        customers = new ArrayList<>();  
+        // Otherwise continue with reading the customers file
+        customers = new ArrayList<>();
         
-        // load the array list with Customer objects created from
-        // the data in the file
+        // If customers file exists, 
+        if (Files.exists(customersPath)) {  // prevent FileNotFoundException
+        	// Open input stream
+        	try (BufferedReader in = new BufferedReader(
+        							 new FileReader(customersFile))) {
+        		// Load array with Customer objects created from data in customers file
+        		String line = in.readLine();
+        		while (line != null) {  // prevent EOFException
+        			String[] columns = line.split(FIELD_SEP);
+        			Customer customer = new Customer(columns[0], columns[1], columns[2]);
+        			System.out.println(customer);
+        			customers.add(customer);
+        			// Read the next line
+        			line = in.readLine();
+        		}
+        	} catch (IOException e) {  // catches IOException
+        		System.out.println("e");
+        		return null;
+        	}
+        }
         
-
-        return customers;            
+        return customers;        
     }
 
     public static Customer getCustomer(String email) {
@@ -61,6 +79,21 @@ public class CustomerTextFile {
 
     private static boolean saveCustomers() {
         // save the Customer objects in the array list to the file
+    	// open input stream for append operation
+    	try (PrintWriter out = new PrintWriter(
+    					  	   new BufferedWriter(
+    					       new FileWriter(customersFile)))) {
+    		// For each Customer object, save it to the file
+    		for (Customer customer : customers) {
+    			String record = customer.getFirstName() + FIELD_SEP + customer.getLastName() 
+    				+ FIELD_SEP + customer.getEmail();
+    			out.println(record);
+    		}
+    	} catch (IOException e) {
+    		System.out.println(e);
+    		return false;
+    	}
+    	
         return true;
     }
 }
